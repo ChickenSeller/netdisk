7ed6106f57e59037e083059435443adf27e7a53b
Kaguya <kaguya@ganxiang.wang>
2016-01-24 00:09:28 +0800

    Complete View

--- resources/views/mainFramework.blade.php
+++ resources/views/mainFramework.blade.php
@@ -15,7 +15,7 @@
     <script src="/js/respond.min.js"></script>
     <![endif]-->
 </head>
-<body style="min-width: 500px;">
+<body style="min-width: 520px;">
 @yield('content')
 <div style="text-align: center">
     <p>
